480438d65a1ed5c98c77a73fcdbdc779fc8b5ac4
АртемQA2603 <artembarabas36@gmail.com>
2022-06-22 19:23:25 +0300

    Повторил материал "Переменные"

---
+++ src/firstlesson.java
@@ -0,0 +1,33 @@
+public class firstlesson{
+    public static void main(String[] args) {
+        byte b1=-100;
+        System.out.println("byte="+b1);
+        short b2=110;
+        System.out.println("short="+b2);
+        int b3=0;
+        System.out.println("int="+b3);
+        float b4=4.5f;
+        System.out.println("float="+b4);
+        double b5=53463446.5346237;
+        System.out.println("double="+b5);
+        boolean artem=true;
+        boolean vika=false;
+        System.out.println("artem and vika="+artem+""+vika);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
